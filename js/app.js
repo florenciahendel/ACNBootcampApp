bc0dd3783cb73be3a54663b7a85dc451fdfda7c3
Florencia <florenciahendel@gmail.com>
2023-08-31 19:03:44 -0300

    adding insertion form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+
+
 let userRole = sessionStorage.getItem('role')
 const button = document.getElementsByClassName(userRole)
 setTimeout(() => {
     button[0].classList.remove('d-none') 
 }, 10);
+
+const idInput= document.getElementById('newBookId');
+
+button[0].addEventListener('click',()=>{
+    let idCounter = localStorage.getItem('bookCounter')||0;
+    idInput.value=idCounter++;
+    localStorage.setItem('bookCounter',idCounter);
+    
+})
+
+
