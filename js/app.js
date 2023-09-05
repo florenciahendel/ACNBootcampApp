67dba5f6845a3818f42a1423abe370ad235b1eee
Florencia <florenciahendel@gmail.com>
2023-09-04 21:36:05 -0300

    Adding DOM elements to app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-
+const idInput= document.getElementById('newBookId'),
+bookName = document.getElementById('inputBookName'), 
+bookAuthor = document.getElementById('inputBookAuthor'),
+bookPublisher = document.getElementById('inputBookPublisher'),
+bookType = document.getElementById('selectBookType'),
+bookState = document.getElementById('bookState'),
+saveBookBtn = document.getElementById('saveBookBtn'),
+cancelBookBtn = document.getElementById('cancelBookBtn');
 
 let userRole = sessionStorage.getItem('role')
-const button = document.getElementsByClassName(userRole)
+const roleBtn = document.getElementsByClassName(userRole)
 setTimeout(() => {
-    button[0].classList.remove('d-none') 
+    roleBtn[0].classList.remove('d-none') 
 }, 10);
 
-const idInput= document.getElementById('newBookId');
-
-button[0].addEventListener('click',()=>{
+roleBtn[0].addEventListener('click',()=>{
     let idCounter = localStorage.getItem('bookCounter')||0;
     idInput.value=idCounter++;
     localStorage.setItem('bookCounter',idCounter);
@@ -16,3 +21,11 @@ button[0].addEventListener('click',()=>{
 })
 
 
+/* Abajo aparecen dos botones
+/Guardar
+Da de alta el libro si todos los campos tienen la información válida. Un pop up aparece con el mensaje: “El ejemplar ha sido dado de alta”y se vuelve a la pantalla de menú de la biblioteca.
+Si algún campo posee data inválida, no se guarda y se marca en rojo el campo con información inválida (un recuadro rojo con el campo). No se borra la data
+Cancelar
+Aparece un pop up de validación con el mensaje: “Al Cancelar se borrarán todos los datos cargados, está seguro?”
+Botón SI, los datos del ejemplar no son guardados y se vuelve a la pantalla de menú de la biblioteca
+Botón No, se vuelve a la pantalla de alta de ejemplares. (se mantienen los datos cargados) */
