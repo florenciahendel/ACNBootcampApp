d0db7bca645e7d626874199e8b1590a8795220ec
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-09-12 16:05:20 -0300

    Add validateBookId function and search button functionality

--- js/reserveBook.js
+++ js/reserveBook.js
@@ -1,20 +1,43 @@
-const btnBuscar = document.getElementById("buscar");
-let valorAbuscar = document.getElementById("inputCodeSearch");
+const btnSearch = document.getElementById("search");
+let IdToSearch = document.getElementById("inputCodeSearch")
 
-async function cargar() {
-    try {
-        const database = await fetch('./js/booksDatabase.json');
-        if (!database.ok) {
-            throw new Error('No se pudo cargar el archivo JSON');
-        }
-        const datos = await database.json();
-      
-        sessionStorage.setItem('libros', JSON.stringify(datos));
-    }
-    catch (error) {
-        console.error('Error al cargar el archivo JSON:', error);
-    }
+
+function validateBookId(booksDB, bookId) {
+    let found = booksDB.find((booksDB) => booksDB.id == bookId);
+
+    if (!found) {
+        return false;
+    } else { 
+        return found;}
+}
+ 
+
+btnSearch.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //Validamos que ambos campos estén completos
+    if (!IdToSearch.value) { //Necesitamos validar el contenido de este campo, entonces usamos el atributo value que es el que nos da el contenido
+        alert('Por favor, agregar un ID para continuar');
+    } else {
+        //Revisamos si el return de la función validarUsuario es un objeto o un boolean. Si es un objeto, fue una validación exitosa y usamos los datos. Si no, informamos por alert.
+        async function fetchDB() {
+            let database = await fetch('./js/booksDatabase.json');
+            let books = await database.json();
+            let data = validateBookId(
+                books,
+                IdToSearch.value //misma lógica que comentario anterior
+            );
+            if (!data) {
+                alert(`ID invalido`);
+            } else {
+sessionStorage.setItem('role', data.role);
+                window.location.href = 'index.html';
+//Acá metemos la lógica para mostrar la info del libro que coincide con el código ingresado           
 
 }
+        }
 
-cargar();
+
+        fetchDB();
+    }
+});
