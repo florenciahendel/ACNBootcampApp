a43d53bc635636942544fd121706184bfc23dbb5
SicilianoFranco21 <sicilianofranco21@gmail.com>
2023-09-12 18:29:54 -0300

    Add behavior that shows a book data after clicking the search button

--- js/reserveBook.js
+++ js/reserveBook.js
@@ -30,10 +30,18 @@ btnSearch.addEventListener('click', (e) => {
             if (!data) {
                 alert(`ID invalido`);
             } else {
-sessionStorage.setItem('role', data.role);
-                window.location.href = 'index.html';
+const bookElement = document.createElement('div');
+bookElement.innerHTML = `
+<h3>${data.name}</h3>
+<p><strong>Id:</strong> ${data.id}</p>
+<p><strong>Autor:</strong> ${data.author}</p>
+<p><strong>Editorial:</strong> ${data.publisher}</p>
+<p><strong>Tipo:</strong> ${data.type}</p>
+<p><strong>Estado:</strong> ${data.state}</p>
+<p><strong>Stock:</strong> ${data.stock}</p>
+`;
+document.body.appendChild(bookElement);
 //Acá metemos la lógica para mostrar la info del libro que coincide con el código ingresado           
-
 }
         }
 
